89ae84523a21223651e3459587fcd3ab1933d70d
MoneyIgos <makowskiigor@gmail.com>
2021-03-05 14:15:06 +0100

    refactor(playlist): move playlist displaying from component to page

--- pages/playlists/index.vue
+++ pages/playlists/index.vue
@@ -1,5 +1,35 @@
 <template>
-  <v-container>
-    <nav-bar />
-  </v-container>
+  <div>
+    <v-card
+      v-for="(playlist, index) in playlists"
+      :key="index"
+      class="mx-auto ma-12"
+      width="800"
+    >
+      <v-img :src="playlist.img"></v-img>
+      <v-card-title>
+        {{ playlist.name }}
+      </v-card-title>
+      <v-card-subtitle>
+        {{ playlist.difficulty }}
+      </v-card-subtitle>
+    </v-card>
+  </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+import IPlaylist from '../../interfaces/playlist';
+
+export default Vue.extend({
+  async asyncData({ $axios }: any) {
+    const playlists = await $axios
+      .$get('http://localhost:8080/playlists')
+      .then(({ playlists }: { playlists: IPlaylist[] }) => {
+        return playlists;
+      });
+    return { playlists };
+  },
+});
+</script>

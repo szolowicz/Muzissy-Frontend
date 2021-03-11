41121d2ff1a980342a8812031c76417fb2c4bc82
MoneyIgos <makowskiigor@gmail.com>
2021-03-11 16:51:20 +0100

    refactor(nuxt.config): change titleTemplate

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors';
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - Muzissy-Frontend',
+    titleTemplate: 'Muzissy',
     title: 'Muzissy-Frontend',
     meta: [
       { charset: 'utf-8' },

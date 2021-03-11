5eb7e5df2dd43b89a0a960f11d1ca410befc8f5e
MoneyIgos <makowskiigor@gmail.com>
2021-03-11 15:12:39 +0100

    refactor(nuxt.config): change primary color

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,7 @@ export default {
       dark: true,
       themes: {
         dark: {
-          primary: colors.blue.darken2,
+          primary: colors.amber.darken3,
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,

2a9a8d443a4f7799d26d132f8c4a3af199f5695b
MoneyIgos <makowskiigor@gmail.com>
2021-03-11 11:33:31 +0100

    feat(_quiz): add fetching data from API

--- pages/playlists/quiz/_quiz.vue
+++ pages/playlists/quiz/_quiz.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <header>
-      <h1>{{ slug.quiz }}</h1>
+      <h1>{{ res.name }}</h1>
     </header>
   </div>
 </template>
@@ -9,10 +9,20 @@
 <script lang="ts">
 import Vue from 'vue';
 export default Vue.extend({
-  data() {
-    return {
-      slug: this.$route.params,
-    };
+  async asyncData({ $axios, params }: any) {
+    let res;
+    try {
+      res = await $axios
+        .$get(`http://localhost:8080/quiz/questions/${params?.quiz}`)
+        .then((res: object) => {
+          console.log(res);
+          return res;
+        });
+    } catch (error) {
+      console.error(error);
+      res = '';
+    }
+    return { res };
   },
 });
 </script>
